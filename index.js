833697761ed417daf4643990e8c82988f5bf49ef
Daniel Schrader <dhs@schrader-wolff-media.de>
2022-02-18 23:28:22 +0100

    Add js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+console.clear();
+
+const bookmarks = document.querySelectorAll('[data-js="bookmark"]');
+
+bookmarks.forEach(bookmark => {
+  bookmark.addEventListener('click', () => {
+    bookmark.classList.toggle('hidden');
+  });
+});
+
+const cards = document.querySelectorAll('[data-js="card"]');
+
+cards.forEach(card => {
+  const answerButton = card.querySelector('[data-js="answerButton"]');
+  const correctAnswer = card.querySelector('[data-js="correctAnswer"]');
+
+  answerButton.addEventListener('click', () => {
+    correctAnswer.classList.toggle('correct');
+  });
+});
